e491a210eeceee8adee1512f06e5026776651332
William <wah20@kent.ac.uk>
2023-11-16 14:45:14 +0000

    Change so it doesn't usea  website but uses MQTT

--- BinESP32Script/BinESP32Script.ino
+++ BinESP32Script/BinESP32Script.ino
@@ -1,79 +1,111 @@
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
-#include <WebServer.h>
+#include <WiFiManager.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
-#include <ESPmDNS.h>
+#include <PubSubClient.h>
 
+const char *ssid = "BLEsensorSetUp";
+const char *password = "password";
+const char *mqtt_server = "test.mosquitto.org";
+const int mqtt_port = 1883;
+const char *mqtt_publish_topic = "uok/iot/wah20/location";
+const char *mqtt_subscribe_topic = "uok/iot/wah20/setLocation";
 
 int Collect_pos = 0;
 int Store_pos = 0;
 int BeaconRSSI;
-WebServer server(80);
+char current_location = ""
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-void handleRoot() {
-  if(abs(BeaconRSSI - Collect_pos) < abs(BeaconRSSI - Store_pos)){
-        server.send(200, "text/plain", "Collect");
-      }else{
-        server.send(200, "text/plain", "Store");
-      }
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  payload[length] = '\0'; // Null-terminate the payload
+  String message = String((char *)payload);
+
+  if (message.equals("collect"))
+  {
+    Collect_pos = BeaconRSSI;
+    Serial.println("Set Collect");
+  }
+  else if (message.equals("store"))
+  {
+    Store_pos = BeaconRSSI;
+    Serial.println("Set Store");
+  }
 }
 
-void SetCollect() {
-  Collect_pos = BeaconRSSI;
-  SetsButton();
-}
-void SetStore() {
-  Store_pos = BeaconRSSI;
-  SetsButton();
-}
-void SetsButton(){
-  server.send(200, "text/html", "<html><body><h1>Setup for bin sensor</h1><p>Bin collect current saved postion: " + String(Collect_pos) + "<form action='/collect' method='get'><button name='button1'>Set Collect position</button></form><p>Bin store current saved postion: " + String(Store_pos) + "</p><form action='/store' method='get'><button name='button2'>Set Store position</button></form></body></html>");
-}
-class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
-  void onResult(BLEAdvertisedDevice advertisedDevice) {
-    if (advertisedDevice.getAddress() == BLEAddress("fd:d3:6f:39:97:b8")) {
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+  void onResult(BLEAdvertisedDevice advertisedDevice)
+  {
+    if (advertisedDevice.getAddress() == BLEAddress("fd:d3:6f:39:97:b8"))
+    {
       BeaconRSSI = advertisedDevice.getRSSI();
-      Serial.println(BeaconRSSI);
     }
   }
 };
 
-void setup() {
-  Serial.begin(115200);
-  setup_wifi();
+void connectMQTT()
+{
+  while (!client.connected())
+  {
+    Serial.println("Attempting MQTT connection...");
+    if (client.connect("BLE_Client"))
+    {
+      Serial.println("Connected to MQTT broker");
+      client.subscribe(mqtt_subscribe_topic);
+    }
+    else
+    {
+      Serial.print("Failed to connect to MQTT, rc=");
+      Serial.print(client.state());
+      Serial.println(" Retrying in 5 seconds...");
+      delay(5000);
+    }
+  }
 }
 
-void setup_wifi() {
-   WiFiManager wm;
-    wm.resetSettings();
-    bool res;
-    res = wm.autoConnect("BLEsensorSetUp","password"); 
-
-  if (!MDNS.begin("esp32config")) {
-    Serial.println("Error setting up MDNS responder!");
-  }
+void setup_wifi()
+{
+  WiFiManager wm;
+  wm.resetSettings();
+  bool res;
+  res = wm.autoConnect(ssid, password);
 
   Serial.println("Connected to WiFi network!");
   Serial.println("Connected!");
-  server.on("/", handleRoot);
-  server.on("/Setup", SetsButton);
-  server.on("/collect", SetCollect);
-  server.on("/store", SetStore);
 
-  server.begin();
-  Serial.println("HTTP server started");
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
+void setup()
+{
+  Serial.begin(115200);
+  setup_wifi();
+  connectMQTT()
+}
 
-void loop() {
-  server.handleClient();
+void loop()
+{
+  if (!client.connected())
+  {
+    connectMQTT();
+  }
+  client.loop();
+
   BLEDevice::init("");
-  BLEScan* pBLEScan = BLEDevice::getScan();
+  BLEScan *pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true);                   // start to scan
-  BLEScanResults _ = pBLEScan->start(5);  // wait for scanning
+  pBLEScan->setActiveScan(true);                    // start to scan
+  BLEScanResults _ = pBLEScan->start(5); // wait for scanning
 
   pBLEScan->clearResults();
-  delay(10);  // Scan every 5 seconds
+  String location = (Collect_pos > Store_pos) ? "collect" : "store";
+  if(current_location != location){
+      client.publish(mqtt_publish_topic, location.c_str());
+      current_location = location;
+  }
+  delay(10); // Scan every 5 seconds
 }
